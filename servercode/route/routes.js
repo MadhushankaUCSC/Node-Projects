6d3176f992f2c43bac67ebadd88326ef3c9a4d22
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2021-04-10 09:57:38 +0530

    item insert

--- servercode/route/routes.js
+++ servercode/route/routes.js
@@ -1,11 +1,32 @@
 var express=require('express');
+const Item=require('../model/shoppingItem');
 var router=express.Router();
 
-router.get('/test',(req,res,next)=>{
-   res.send('Testing Route');
+router.get('/items',(req,res,next)=>{
+Item.find(function(err,items){
+   if(err){
+      res.json(err);
+   }else{
+      res.json(items);
+   }
+})
 });
 
-router.post('/test',(req,res,next)=>{
+router.post('/item',(req,res,next)=>{
+let newShoppingItem=new Item({
+   itemName:req.body.itemName,
+   itemQuantity:req.body.itemQuantity,
+   itemBought:req.body.itemBought,
+   
+});
+
+newShoppingItem.save((err,item)=>{
+   if(err){
+      res.json(err);
+   }else{
+      res.json({msg:'Item has been added to db'});
+   }
+});
 
 });
 
